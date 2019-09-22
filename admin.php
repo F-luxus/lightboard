afec477c2114da2102eb72367027625aafd74666
Tomas Kleinauskas <kleinauskastom@gmail.com>
2019-09-22 22:33:38 +0300

    Formos atskirtos viena nuo kitos

--- admin.php
+++ admin.php
@@ -15,7 +15,7 @@ if (!isset($_GET['id'])){
     echo '
     <form action="admin.php?id=login" method="post">
     <label>Įveskite specialisto vardą: 
-    <input type=text name="name" autocomplete="off" pattern="[a-žA-Ž]+" required placeholder="Specialisto vardas"></input></label>
+    <input type=text name="name" autocomplete="off" pattern="[a-žA-Ž]+" required placeholder="Specialisto vardas"></input></label><br>
     <label>Įveskite specialisto kodą :  <input type=password name="pass" autocomplete="off" pattern="[0-9]+" required placeholder="Specialisto kodas"></input></label>
     <button>Prisijungti</button><br>
     ';    
